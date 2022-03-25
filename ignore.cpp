9a50047962547aff82942bf47fbb3f10a2c7d26e
Alexander-col <aexro2221@gmail.com>
2022-03-25 12:04:28 -0400

    the original cesar cypher shfiting is fixed so now it can loop around the approptiate ACSI values when ddealing with upper or lower case letters

--- ignore.cpp
+++ ignore.cpp
@@ -1,107 +1,41 @@
+// type "man ascii" in the terminal). 
 #include <iostream>
-#include <string>
-#include "caesar.h"
-#include "funcs.h"
-char shiftChar(char c, int rshift)
+// #include <string>
+// #include "caesar.h"
+// #include "funcs.h"
+char shiftChar(char c, int rshift) // Leave more comments for your future self so you can understand work!
 {
     int a = int(c);
     if ((a >= 65 && a <= 90) || (a >= 97 && a <= 122))
     {
         int value = int(c) + rshift;
-        if(value == 91)
+        if (a >= 65 && a <= 90)
         {
-            return 'A';
+            if (value > 90)
+            {   
+                int diff = 90 - int(c);
+                diff = rshift - diff;
+                value = 64 + diff;
+                return value;
+            }
+            return value;
         }
-        else if (value == 92)
+        if(a >= 97 && a <= 122)
         {
-            return 'B';
-        }
-        else if (value == 93)
-        {
-            return 'C';
-        }
-        else if (value == 94)
-        {
-            return 'D';
-        }
-        else if (value == 95)
-        {
-            return 'E';
-        }
-        else if (value == 96)
-        {
-            return 'F';
-        }
-        else if (value == 97)
-        {
-            return 'G';
-        }
-        else if (value == 98)
-        {
-            return 'H';
-        }
-        else if (value == 99)
-        {
-            return 'I';
-        }
-        else if (value == 100)
-        {
-            return 'J';
-        }
-        else if (value == 101)
-        {
-            return 'K';
+            if (value > 122)
+            {
+                int diff = 122 - int(c);
+                diff = rshift - diff;
+                value = 96 + diff;
+                return value;
+            }
+            return value;
         }
 
-        else if (value == 123)
-        {
-            return 'a';
-        }
-        else if (value == 124)
-        {
-            return 'b';
-        }
-        else if (value == 125)
-        {
-            return 'c';
-        }
-        else if (value == 126)
-        {
-            return 'd';
-        }
-        else if (value == 127)
-        {
-            return 'F';
-        }
-        else if (value == 128)
-        {
-            return 'G';
-        }
-        else if (value == 129)
-        {
-            return 'H';
-        }
-        else if (value == 130)
-        {
-            return 'I';
-        }
-        else if (value == 131)
-        {
-            return 'J';
-        }
-        else if (value == 132)
-        {
-            return 'K';
-        }
-        else if (value == 133)
-        {
-            return 'L';
-        }
+
         return value;
+
     }
-
-    
-
     return c;
 }
 
