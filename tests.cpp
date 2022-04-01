332337f8c79d8cbc7ec80868e55bbbe73270ecc7
Alexander-col <aexro2221@gmail.com>
2022-04-01 07:02:49 -0400

    added tests

--- tests.cpp
+++ tests.cpp
@@ -3,3 +3,11 @@
 #include "funcs.h"
 
 // add your tests here
+
+TEST_CASE("Checking for Cesar func:" ) 
+{ 
+    CHECK(encryptCaesar("Way to Go!", 5) == "Bfd yt Lt!");
+    CHECK(encryptCaesar("Hello, World!" , 10) == "Rovvy, Gybvn!");
+    CHECK(encryptCaesar("Call Me If You Get Lost", 2) == "Ecnn Og Kh Aqw Igv Nquv");
+}
+
